8a19482ba0e993b041c897285a456133044c247f
Brian Nguyen <briannguyencoco@gmail.com>
2022-07-23 12:52:51 -0500

    working player name selection and transition after choosing

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
-console.log('hi')
-const board = document.querySelector('.board')
+let gameData = {
+    board: new Array(9),
+    player1: '',
+    player2: '',
+    currentPlayer: '',
+};
+
+const body = document.querySelector('body');
+const players = document.querySelector('.players');
+//body.removeChild(players);
+
+const confirm = document.querySelector('#confirm');
+confirm.addEventListener('click', () => {
+    gameData.player1 = document.querySelector('.player1').value;
+    gameData.player2 = document.querySelector('.player2').value;
+    gameData.currentPlayer = gameData.player1;
+    body.removeChild(players);
+    
+});
+
+const board = document.querySelector('.board');
 for(let i = 0; i < 9; i++) {
-    const div = document.createElement('div')
-    div.classList.add(`b${i}`)
-    div.classList.add('box')
-    board.appendChild(div)
+    const div = document.createElement('div');
+    div.classList.add(`b${i}`);
+    div.classList.add('box');
+    board.appendChild(div);
     div.addEventListener('click', () => {
         div.textContent = "X";
     });
 }
+
