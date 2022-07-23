3d31bb02fafc7556323ec5298d80e386335d4073
Brian Nguyen <briannguyencoco@gmail.com>
2022-07-23 15:52:32 -0500

    added color changing on winning row

--- script.js
+++ script.js
@@ -25,7 +25,51 @@ for(let i = 0; i < 9; i++) {
     div.classList.add('box');
     board.appendChild(div);
     div.addEventListener('click', () => {
-        div.textContent = "X";
+        if(gameData.currentPlayer == gameData.player1) {
+            div.textContent = 'X';
+            gameData.board[i] = "X";
+            winner('X')
+            gameData.currentPlayer = gameData.player2;
+            
+        }
+        else {
+            div.textContent = 'O';
+            gameData.board[i] = "O";
+            winner('O');
+            gameData.currentPlayer = gameData.player1;
+    
+        }
+
     });
 }
 
+function updateBoard() {
+    for(let i = 0; i < 9; i++) {
+        const div = document.querySelector(`b${i}`);
+        div.textContent = gameData.board[i];
+    }
+}
+
+function winner(playerSymbol) {
+    const winCombos = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6], 
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+        ];
+        winCombos.forEach((combo, index) => {
+            if(gameData.board[combo[0]] == playerSymbol && gameData.board[combo[1]] == playerSymbol && gameData.board[combo[2]] == playerSymbol) {
+                for(let i = 0; i < 9; i++) {
+                    const div = document.querySelector(`.b${i}`);
+                    //console.log(div);
+                    if (i === combo[0] || i === combo[1] || i === combo[2]) {
+                        div.classList.add('winner');
+                    }
+                }
+            }
+        });
+}
